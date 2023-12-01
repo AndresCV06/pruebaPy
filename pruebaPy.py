ae531a6bec7e6d422a77c31c0187938b3fc25c8a
AndresCV06 <andrescorralesv@hotmail.com>
2023-12-01 00:54:20 -0600

    prueba2

--- pruebaPy.py
+++ pruebaPy.py
@@ -1,1 +1,8 @@
 print("Esto es una prueba")
+#"git status" sirve para ver el estatus actual del repositorio y si se le ha hecho algún cambio
+
+#"git add" sirve para agregar los cambios a una pila de trabajo, se le debe especificar el archivo
+#ejemplo git add pruebaPy.py
+
+for x in range(3):
+    print(x)
